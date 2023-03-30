be3d6e6320bb45639e542c5f3d66a2157ba2dfe0
Julien Rouhaud <julien.rouhaud@free.fr>
2023-03-30 09:20:17 +0800

    Improve partitioning / inheritance regression tests.
    Regression tests now cover, for both partitioning and inheritance tables:
    - partitionining with different columns orders
    - multi level partitionining
    - multi level inheritance
    - multi ancestors inheritance
    
    Those tests are not really exercising anything new in the code for now, but it
    will as soon as we add support for inheritance of SECURITY LABELs for
    partitioning / inheritance tables.

--- test/sql/02_partitioning.sql
+++ test/sql/02_partitioning.sql
@@ -7,8 +7,10 @@ SECURITY LABEL FOR pg_anonymize ON ROLE :current_user IS 'anonymize';
 -- list partitioning
 --------------------
 CREATE TABLE t_part_list(id integer, val text) PARTITION BY LIST (id);
-CREATE TABLE t_part_list_1 PARTITION OF t_part_list FOR VALUES IN (1);
-CREATE TABLE t_part_list_2 PARTITION OF t_part_list FOR VALUES IN (2);
+CREATE TABLE t_part_list_1(val text, id integer);
+ALTER TABLE t_part_list ATTACH PARTITION t_part_list_1 FOR VALUES IN (1);
+CREATE TABLE t_part_list_2(val text, id integer);
+ALTER TABLE t_part_list ATTACH PARTITION t_part_list_2 FOR VALUES IN (2);
 INSERT INTO t_part_list SELECT i, 'line ' || i FROM generate_series(1, 2) i;
 SECURITY LABEL FOR pg_anonymize
     ON COLUMN public.t_part_list.val IS $$'root hidden'::text$$;
@@ -38,8 +40,10 @@ COPY t_part_list_2 TO stdout;
 -- range partitioning
 ---------------------
 CREATE TABLE t_part_range(id integer, val text) PARTITION BY RANGE (id);
-CREATE TABLE t_part_range_1_3 PARTITION OF t_part_range FOR VALUES FROM (1) TO (3);
-CREATE TABLE t_part_range_3_5 PARTITION OF t_part_range FOR VALUES FROM (3) TO (5);
+CREATE TABLE t_part_range_1_3(val text, id integer);
+ALTER TABLE t_part_range ATTACH PARTITION t_part_range_1_3 FOR VALUES FROM (1) TO (3);
+CREATE TABLE t_part_range_3_5(val text, id integer);
+ALTER TABLE t_part_range ATTACH PARTITION t_part_range_3_5 FOR VALUES FROM (3) TO (5);
 INSERT INTO t_part_range SELECT i, 'line ' || i FROM generate_series(1, 4) i;
 SECURITY LABEL FOR pg_anonymize
     ON COLUMN public.t_part_range.val IS $$'root hidden'::text$$;
@@ -64,3 +68,53 @@ COPY t_part_range_1_3 TO stdout;
 -- unless there's an explicit anonymization rule on it
 SELECT * FROM t_part_range_3_5 ORDER BY id;
 COPY t_part_range_3_5 TO stdout;
+
+----------------------
+-- nested partitioning
+----------------------
+-- root level
+CREATE TABLE t_part_nest(id integer, id2 integer, val text)
+    PARTITION BY RANGE (id);
+-- level 1
+CREATE TABLE t_part_nest_1(id2 integer, id integer, val text);
+ALTER TABLE t_part_nest ATTACH PARTITION t_part_nest_1 FOR VALUES FROM (1) TO (2);
+CREATE TABLE t_part_nest_23(val text, id2 integer, id integer) PARTITION BY LIST (id);
+ALTER TABLE t_part_nest ATTACH PARTITION t_part_nest_23 FOR VALUES FROM (2) TO (4);
+-- level 2
+CREATE TABLE t_part_nest_23_2(val text, id integer, id2 integer);
+ALTER TABLE t_part_nest_23 ATTACH PARTITION t_part_nest_23_2 FOR VALUES IN (2);
+CREATE TABLE t_part_nest_23_3(val text, id integer, id2 integer);
+ALTER TABLE t_part_nest_23 ATTACH PARTITION t_part_nest_23_3 FOR VALUES IN (3);
+
+INSERT INTO t_part_nest SELECT i, i, 'line ' || i FROM generate_series(1, 3) i;
+SECURITY LABEL FOR pg_anonymize
+    ON COLUMN t_part_nest.val IS $$'root hidden'::text$$;
+SECURITY LABEL FOR pg_anonymize
+    ON COLUMN t_part_nest_23.val IS $$'nested hidden'::text$$;
+SECURITY LABEL FOR pg_anonymize
+    ON COLUMN t_part_nest_23_3.id2 IS $$42$$;
+
+SET pg_anonymize.enabled = 'off';
+
+-- should see original data
+SELECT * FROM t_part_nest ORDER BY id;
+SELECT * FROM t_part_nest_1 ORDER BY id;
+SELECT * FROM t_part_nest_23 ORDER BY id;
+SELECT * FROM t_part_nest_23_2 ORDER BY id;
+SELECT * FROM t_part_nest_23_3 ORDER BY id;
+
+SET pg_anonymize.enabled = 'on';
+
+-- should see anonymized data when selecting from root partition
+SELECT * FROM t_part_nest ORDER BY id;
+COPY t_part_nest TO stdout;
+-- but original data from any leaf / subpartition
+SELECT * FROM t_part_nest_1 ORDER BY id;
+COPY t_part_nest_1 TO STDOUT;
+SELECT * FROM t_part_nest_23_2 ORDER BY id;
+COPY t_part_nest_23_2 TO STDOUT;
+-- unless there's an explicit anonymization rule for it
+SELECT * FROM t_part_nest_23 ORDER BY id;
+COPY t_part_nest_23 TO stdout;
+SELECT * FROM t_part_nest_23_3 ORDER BY id;
+COPY t_part_nest_23_3 TO stdout;
--- test/sql/03_inheritance.sql
+++ test/sql/03_inheritance.sql
@@ -4,17 +4,23 @@ SELECT current_user \gset
 SECURITY LABEL FOR pg_anonymize ON ROLE :current_user IS 'anonymize';
 
 CREATE TABLE t_inh(id integer, val text);
-CREATE TABLE t_inh_a (val_a text) INHERITS (t_inh);
+CREATE TABLE t_inh_a (val_a text, id integer) INHERITS (t_inh);
 CREATE TABLE t_inh_b (val_b text) INHERITS (t_inh);
+-- multi inheritance
+CREATE TABLE t_inh_ab_c (id integer, val_a text, val_c text)
+    INHERITS (t_inh_a, t_inh_b);
 
 INSERT INTO t_inh SELECT 1, 't_inh';
 INSERT INTO t_inh_a SELECT 2, 't_inh_a', 'a';
 INSERT INTO t_inh_b SELECT 3, 't_inh_b', 'b';
+INSERT INTO t_inh_ab_c SELECT 4, 't_inh_c', 't_inh_c', 't_inh_c', 'c';
 
 SECURITY LABEL FOR pg_anonymize
     ON COLUMN public.t_inh.val IS $$'root hidden'::text$$;
 SECURITY LABEL FOR pg_anonymize
     ON COLUMN public.t_inh_b.val IS $$'part hidden'::text$$;
+SECURITY LABEL FOR pg_anonymize
+    ON COLUMN public.t_inh_ab_c.val_c IS $$'part c hidden'::text$$;
 
 SET pg_anonymize.enabled = 'off';
 
@@ -22,6 +28,7 @@ SET pg_anonymize.enabled = 'off';
 SELECT * FROM t_inh ORDER BY id;
 SELECT * FROM t_inh_a ORDER BY id;
 SELECT * FROM t_inh_b ORDER BY id;
+SELECT * FROM t_inh_ab_c ORDER BY id;
 
 SET pg_anonymize.enabled = 'on';
 
@@ -34,3 +41,5 @@ COPY t_inh_a TO stdout;
 -- unless there's an explicit anonymization rule on it
 SELECT * FROM t_inh_b ORDER BY id;
 COPY t_inh_b TO stdout;
+SELECT * FROM t_inh_ab_c ORDER BY id;
+COPY t_inh_ab_c TO stdout;
