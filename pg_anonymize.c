6d8eed5abbe00eea27e4c93858612f2b2d9a68de
Julien Rouhaud <julien.rouhaud@free.fr>
2024-12-15 12:44:23 +0800

    Bump copyright year.

--- pg_anonymize.c
+++ pg_anonymize.c
@@ -5,7 +5,7 @@
  *
  *
  * pg_anonymize
- * Copyright (C) 2022-2023 - Julien Rouhaud.
+ * Copyright (C) 2022-2024 - Julien Rouhaud.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
